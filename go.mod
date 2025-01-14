d761cddb9d05d2412393d9181387548f6ba24d14
FelipeAugst <felipeaug92@gmail.com>
2025-01-13 23:44:54 -0300

    start

---
+++ go.mod
@@ -0,0 +1,28 @@
+module oauth_server
+
+go 1.23.1
+
+require github.com/go-oauth2/mysql v3.0.4+incompatible
+
+require github.com/jmoiron/sqlx v1.2.0 // indirect
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-oauth2/oauth2 v3.9.2+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/imrenagi/go-oauth2-mysql v1.1.0
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tidwall/btree v0.0.0-20170113224114-9876f1454cf0 // indirect
+	github.com/tidwall/buntdb v1.1.0 // indirect
+	github.com/tidwall/gjson v1.3.2 // indirect
+	github.com/tidwall/grect v0.0.0-20161006141115-ba9a043346eb // indirect
+	github.com/tidwall/match v1.0.1 // indirect
+	github.com/tidwall/pretty v1.0.0 // indirect
+	github.com/tidwall/rtree v0.0.0-20180113144539-6cd427091e0e // indirect
+	github.com/tidwall/tinyqueue v0.0.0-20180302190814-1e39f5511563 // indirect
+	gopkg.in/gorp.v2 v2.2.0 // indirect
+	gopkg.in/oauth2.v3 v3.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-oauth2/mysql"
+	"github.com/go-oauth2/oauth2/manage"
+	"github.com/go-oauth2/oauth2/models"
+	"github.com/go-oauth2/oauth2/server"
+	"github.com/go-oauth2/oauth2/store"
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/oauth2.v3"
+	"gopkg.in/oauth2.v3/errors"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "felipe:felipe@/goauth2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	storage := mysql.NewStoreWithDB(db, "tokens", 0)
+	m := manage.NewDefaultManager()
+	m.MustTokenStorage(storage, nil)
+
+	clientStorage := store.NewClientStore()
+	var client = models.Client{ID: "Client", Secret: "secret", UserID: "user3", Domain: "http://localhost"}
+	clientStorage.Set("Client", &client)
+
+	m.MapClientStorage(clientStorage)
+	srv := server.NewDefaultServer(m)
+	srv.SetAllowedGrantType(oauth2.ClientCredentials)
+
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+
+	srv.SetInternalErrorHandler(server.InternalErrorHandler(func(er error) (re *errors.Response) {
+		return errors.NewResponse(err, 500)
+	}))
+
+	TokenHandler := func(w http.ResponseWriter, r *http.Request) {
+
+		srv.HandleTokenRequest(w, r)
+
+	}
+
+	ProtectedHandler := func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("Entering protected route!"))
+
+	}
+
+	AuthMiddleware := func(next http.HandlerFunc) http.HandlerFunc {
+
+		return func(w http.ResponseWriter, r *http.Request) {
+			token := r.Header.Get("Authorization")
+			fmt.Println(token)
+			if token == "" {
+				w.WriteHeader(400)
+				w.Write([]byte("Invalid token"))
+				return
+			}
+			t := strings.Split(token, " ")
+
+			if t[0] != "Bearer" {
+				w.WriteHeader(400)
+				w.Write([]byte("Not a bearer token"))
+				return
+
+			}
+
+			client, err := srv.ValidationBearerToken(r)
+			if err != nil {
+				w.WriteHeader(400)
+				w.Write([]byte(err.Error()))
+				return
+
+			}
+
+			json.NewEncoder(w).Encode(client)
+			next(w, r)
+
+		}
+
+	}
+
+	http.Handle("POST /token/", http.HandlerFunc(TokenHandler))
+	http.Handle("/", http.HandlerFunc(AuthMiddleware(ProtectedHandler)))
+	http.ListenAndServe(":5000", nil)
+
+}
